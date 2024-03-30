6ba66d4dad846f8fe989d2dc937f5a1d57726cfd
Şenol Ataç <senolatac@Senols-MBP.home>
2024-03-30 17:54:54 +0300

    initial

---
+++ src/test/java/com/example/demoadminui/DemoAdminUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoadminui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAdminUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
